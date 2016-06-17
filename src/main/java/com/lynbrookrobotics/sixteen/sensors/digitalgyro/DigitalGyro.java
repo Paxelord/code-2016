3d6fe048f2ad7f23f7055d2c3c84b9292b57d0e2
Philip Axelrod <philip.axelrod.web@gmail.com>
2016-06-16 19:01:45 -0700

    Add method to get position at a time

--- src/main/java/com/lynbrookrobotics/sixteen/sensors/digitalgyro/DigitalGyro.java
+++ src/main/java/com/lynbrookrobotics/sixteen/sensors/digitalgyro/DigitalGyro.java
@@ -10,11 +10,12 @@ public abstract class DigitalGyro {
   Value3D currentVelocity = new Value3D(0, 0, 0);
   Value3D currentPosition = new Value3D(0, 0, 0);
 
-  ArrayList<Value3DLogEntry> angleLog;
+  private ArrayList<Value3DLogEntry> angleLog = new ArrayList<>();
+  private ArrayList<Long> angleLogTimes = new ArrayList<>(200);
 
   Value3D currentDrift;
 
-  ArrayList<Value3D> values = new ArrayList<>(200);
+  private ArrayList<Value3D> values = new ArrayList<>(200);
   int index = 0;
   boolean calibrating = true;
 
@@ -59,7 +60,8 @@ public abstract class DigitalGyro {
             trapaziodalIntegration(currentVelocity.valueY(), previousVelocity.valueY()),
             trapaziodalIntegration(currentVelocity.valueZ(), previousVelocity.valueZ())
         ));
-    angleLog.add(new Value3DLogEntry(currentPosition, System.currentTimeMillis()));
+    angleLog.add(new Value3DLogEntry(currentPosition, toNearestLowerMultipleTickPeriod(
+        System.currentTimeMillis()) ) );
   }
 
   public Value3D currentPosition() {
@@ -72,13 +74,39 @@ public abstract class DigitalGyro {
 
   /**
    * Integrates a single gyro axis.
-   * Takes the average of the 2 velocities and multiplies by tick period (20 ms)
-   *
+   * Takes the average of the 2 velocities and multiplies by tick period (5 ms)
    * @param velocity         The current velocity read from the gyro
    * @param previousVelocity the previous velocity read from the gyro
    * @return The angle gotten by taking the integral of the angular velocity
    */
   private double trapaziodalIntegration(double velocity, double previousVelocity) {
     return (RobotConstants.TICK_PERIOD * ((velocity + previousVelocity) / 2));
+  }
+
+  /**
+   * Return an instance of Value3D corresponding to the current angular position at a give time in
+   * milliseconds
+   * @param atTime the time to get the angular position in milliseconds since the initialization of
+   * System. values is rounded to the nearest lower multiple of 5, since our tickperiod is 5
+   * milliseconds
+   * @return Angular position at the given time
+   */
+  public Value3D getPostionAtTime(long atTime) {
+    if ( angleLogTimes.contains( toNearestLowerMultipleTickPeriod(atTime) ) ) {
+      return angleLog.get( angleLogTimes.indexOf(atTime) ).getValue3DEntry();
+    }
+
+    return null;
+  }
+
+  /**
+   * Round number down to the nearest lower multiple of of the robot tick period. For example to
+   * toNearestLowerMultipleTickPeriod(11) returns 10 and toNearestLowerMultipleTickPeriod(19)
+   * returns 15
+   * @param toRound what to round  down to the nearest multiple of the robot tick period
+   * @return toRound rounded to the nearest lower multiple of 5
+   */
+  public Long toNearestLowerMultipleTickPeriod(Long toRound) {
+    return toRound - toRound % (long) RobotConstants.TICK_PERIOD;
   }
 }
